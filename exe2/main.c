ae15c96c2f5e96cf32cb4421deed47a3a4c11335
Alison Araujo <alisonao@al.insper.edu.br>
2025-02-19 13:35:33 -0300

    fix 2 & 4

--- exe2/main.c
+++ exe2/main.c
@@ -2,20 +2,20 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int LED_PIN = 4;
+const int LED_PIN_R = 4;
 const int BTN_PIN_R = 28;
 volatile char btn_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x8) { // fall edge
+  if (events == 0x4) { // fall edge
     btn_flag = 1;
   }
 }
 
 int main() {
   stdio_init_all();
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
   gpio_init(BTN_PIN_R);
   gpio_set_dir(BTN_PIN_R, GPIO_IN);
   gpio_pull_up(BTN_PIN_R);
@@ -24,8 +24,8 @@ int main() {
 
   while (true) {
     if (btn_flag == 1) {
-      int led_state = gpio_get(LED_PIN);
-      gpio_put(LED_PIN, !led_state);
+      bool led_state = gpio_get(LED_PIN_R);
+      gpio_put(LED_PIN_R, !led_state);
       btn_flag = 0;
     }
   }
--- exe4/main.c
+++ exe4/main.c
@@ -2,9 +2,53 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+const int BTN_PIN_G = 26;
+const int BTN_PIN_R = 28;
+volatile char red_flag = 0;
+volatile char green_flag = 0;
+
+void btn_callback(uint gpio, uint32_t events) {
+  if (events == 0x4) { // fall edge
+    if (gpio == BTN_PIN_R){
+      red_flag = 1;
+    }
+    else if (gpio == BTN_PIN_G){
+      green_flag = 1;
+    }
+  }
+}
+
 int main() {
   stdio_init_all();
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
 
+  // callback led r (first)
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                          &btn_callback);
+
+  // callback led g (nao usar _with_callback)
+  gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
   while (true) {
+    if (red_flag == 1) {
+      int led_state_r = gpio_get(LED_PIN_R);
+      gpio_put(LED_PIN_R, !led_state_r);
+      red_flag = 0;
+    }
+    if (green_flag == 1) {
+      int led_state_g = gpio_get(LED_PIN_G);
+      gpio_put(LED_PIN_G, !led_state_g);
+      green_flag = 0;
+    }
   }
 }
